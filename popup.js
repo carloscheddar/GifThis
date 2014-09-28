6be415b330138b1fc995c7ac26c74cda1438490a
Carlos Feliciano-Barba <c.feliciano2009@gmail.com>
2014-09-27 22:19:04 -0400

    Get search value from input form.

--- popup.js
+++ popup.js
@@ -1,18 +1,21 @@
 $(document).ready(function(){
-   $('body').on('click', 'a', function(){
-     chrome.tabs.create({url: $(this).attr('href')});
-     return false;
-   });
+  $('body').on('click', 'a', function(){
+   chrome.tabs.create({url: $(this).attr('href')});
+   return false;
+  });
 
-  var query = 'korra';
-  var limit = 5;
+  $("form").submit(function(e){
+    e.preventDefault();
 
-  var xhr = $.get("http://api.giphy.com/v1/gifs/search?q="+ query + "&api_key=dc6zaTOxFJmzC&limit="+ limit);
-  xhr.done(function(data) {
+    var query = $('form').serializeArray()[0].value;
+    var limit = 5;
 
-    _.each(data.data, function (gif) {
-      imageUrl = gif.images.downsized.url;
-      $(".gifs").append('<li><a href="'+ gif.url +'"><img src='+ imageUrl +'></img></a></li>');
+    var xhr = $.get("http://api.giphy.com/v1/gifs/search?q="+ query + "&api_key=dc6zaTOxFJmzC&limit="+ limit);
+    xhr.done(function(data) {
+      _.each(data.data, function (gif) {
+        imageUrl = gif.images.downsized.url;
+        $(".gifs").append('<li><a href="'+ gif.url +'"><img src='+ imageUrl +'></img></a></li>');
+      });
     });
   });
 });
