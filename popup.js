65c971f2c5920c6e4cd9e8e09ced7db397b1dc6c
Carlos Feliciano-Barba <c.feliciano2009@gmail.com>
2014-09-27 22:43:22 -0400

    Empty the list on each submit and show message if results are empty.

--- popup.js
+++ popup.js
@@ -1,17 +1,29 @@
 $(document).ready(function(){
+  // Open a new tab if the user clicks on an image
   $('body').on('click', 'a', function(){
    chrome.tabs.create({url: $(this).attr('href')});
    return false;
   });
 
+  // Get the search value on submit
   $("form").submit(function(e){
     e.preventDefault();
 
-    var query = $('form').serializeArray()[0].value;
-    var limit = 5;
+    // Empty the list on each request
+    $('.gifs').empty();
 
+    var query = $('form').serializeArray()[0].value;
+    var limit = 6;
+
+    // Get the gifs from the giphy api
     var xhr = $.get("http://api.giphy.com/v1/gifs/search?q="+ query + "&api_key=dc6zaTOxFJmzC&limit="+ limit);
     xhr.done(function(data) {
+      //Return if result is empty
+      if (data.pagination.count === 0) {
+        $(".gifs").append('<li><p>No gif found.</p></li>');
+        return;
+      }
+
       _.each(data.data, function (gif) {
         imageUrl = gif.images.downsized.url;
         $(".gifs").append('<li><a href="'+ gif.url +'"><img src='+ imageUrl +'></img></a></li>');
