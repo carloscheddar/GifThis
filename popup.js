9bcfd1d9f87b929f1a6ae3f8bb2c012f84b2e67d
Carlos Feliciano-Barba <c.feliciano2009@gmail.com>
2014-09-27 20:36:34 -0400

    Display gif images on the extension.

--- popup.js
+++ popup.js
@@ -1,10 +1,18 @@
-var query = 'kittens';
-var limit = 5;
+$(document).ready(function(){
+   $('body').on('click', 'a', function(){
+     chrome.tabs.create({url: $(this).attr('href')});
+     return false;
+   });
 
-var xhr = $.get("http://api.giphy.com/v1/gifs/search?q="+ query + "&api_key=dc6zaTOxFJmzC&limit="+ limit);
-xhr.done(function(data) {
+  var query = 'korra';
+  var limit = 5;
 
-  _.each(data.data, function (gif) {
-    $(".gifs").append('<li><a href="'+ gif.url +'">Gif</a></li>');
+  var xhr = $.get("http://api.giphy.com/v1/gifs/search?q="+ query + "&api_key=dc6zaTOxFJmzC&limit="+ limit);
+  xhr.done(function(data) {
+
+    _.each(data.data, function (gif) {
+      imageUrl = gif.images.downsized.url;
+      $(".gifs").append('<li><a href="'+ gif.url +'"><img src='+ imageUrl +'></img></a></li>');
+    });
   });
 });
