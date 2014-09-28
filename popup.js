5c65a52467ebcdb5d4e0ea25da7f458413d9fc96
Carlos Feliciano-Barba <c.feliciano2009@gmail.com>
2014-09-27 20:22:00 -0400

    Show gif links

--- popup.js
+++ popup.js
@@ -1,1 +1,10 @@
-console.log("Hello, World");
+var query = 'kittens';
+var limit = 5;
+
+var xhr = $.get("http://api.giphy.com/v1/gifs/search?q="+ query + "&api_key=dc6zaTOxFJmzC&limit="+ limit);
+xhr.done(function(data) {
+
+  _.each(data.data, function (gif) {
+    $(".gifs").append('<li><a href="'+ gif.url +'">Gif</a></li>');
+  });
+});
