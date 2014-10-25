8e5b3a5424981a9929c406c8f186d469597f075d
Carlos Feliciano <c.feliciano2009@gmail.com>
2014-10-25 17:34:29 -0400

    Add trending and random gif queries using packery.

--- assets/coffee/main.coffee
+++ assets/coffee/main.coffee
@@ -1,54 +1,82 @@
-makeItemElem= (data, container, fragment) ->
+# Method for showing and hiding the loading gif
+Loading =
+  show: ->
+    $(".loading").removeClass "is-hidden"
+  hide: ->
+    $(".loading").addClass "is-hidden"
 
-  item = document.createElement("div")
-  item.className = "item is-hidden"
-  img = document.createElement("img")
+# Only add image to packer once it's loaded
+loadImages= ($container)->
+  imgLoad = imagesLoaded($container)
+  imgLoad.on "progress", (imgLoad, image) ->
+    Loading.hide()
+    return  unless image.isLoaded
+    itemElem = image.img.parentNode
+    classie.remove itemElem, "is-hidden"
+    $container.packery('appended', itemElem)
+    $container.packery()
 
-  console.log data
-  img.src = data.data.image_url
-  item.appendChild img
-  fragment.appendChild item
-  container.appendChild fragment
-
+# Remove items from container
+removeItems= ->
+  $('.item').remove().promise()
 
 createItems = (results, container) ->
+  fragment = document.createDocumentFragment()
+  _.each results, (data) ->
+    item = document.createElement("div")
+    item.className = "item is-hidden"
+    img = document.createElement("img")
+
+    img.src = data.images.downsized.url
+    item.appendChild img
+    fragment.appendChild item
+    container.append fragment
+
+randomItems = (results, container) ->
   fragment = document.createDocumentFragment()
   _.each results, (response) ->
     item = document.createElement("div")
     item.className = "item is-hidden"
     img = document.createElement("img")
 
-    console.log response
     img.src = response.data.image_url
     item.appendChild img
     fragment.appendChild item
-    container.appendChild fragment
+    container.append fragment
+
+getTrending = ($container) ->
+  limit = 25
+  $.get("http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC&limit=" + limit)
+  .done (results) ->
+    createItems results.data, $container
+    loadImages($container)
 
 
-docReady ->
+getRandom = ($container) ->
   results = {}
-  container = document.querySelector("#container")
-
-  promise = $.when _.times 20, (n) ->
+  promise = $.when _.times 10, (n) ->
     $.get("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC")
     .done (data) ->
       results[n] = data
 
-
   promise.done (data) ->
     $.when.apply($, data).done ->
-      createItems results, container
-      imgLoad = imagesLoaded(container)
-      imgLoad.on "progress", (imgLoad, image) ->
-        return  unless image.isLoaded
-        itemElem = image.img.parentNode
-        classie.remove itemElem, "is-hidden"
-        pckry.appended itemElem
-        pckry.layout()
+      randomItems results, $container
+      loadImages($container)
 
-  pckry = new Packery(container,
+docReady ->
+  $container = $('#container')
+
+  $('.trending').on 'click', ->
+    removeItems()
+    Loading.show()
+    getTrending($container)
+
+  $('.random').on 'click', ->
+    removeItems()
+    Loading.show()
+    getRandom($container)
+
+  $container.packery
     itemSelector: ".item"
-    gutter: 2,
-    "columnWidth": 196,
-  )
-
+    gutter: 2
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,50 @@
-var createItems, makeItemElem;
+var Loading, createItems, getRandom, getTrending, loadImages, randomItems, removeItems;
 
-makeItemElem = function(data, container, fragment) {
-  var img, item;
-  item = document.createElement("div");
-  item.className = "item is-hidden";
-  img = document.createElement("img");
-  console.log(data);
-  img.src = data.data.image_url;
-  item.appendChild(img);
-  fragment.appendChild(item);
-  return container.appendChild(fragment);
+Loading = {
+  show: function() {
+    return $(".loading").removeClass("is-hidden");
+  },
+  hide: function() {
+    return $(".loading").addClass("is-hidden");
+  }
+};
+
+loadImages = function($container) {
+  var imgLoad;
+  imgLoad = imagesLoaded($container);
+  return imgLoad.on("progress", function(imgLoad, image) {
+    var itemElem;
+    Loading.hide();
+    if (!image.isLoaded) {
+      return;
+    }
+    itemElem = image.img.parentNode;
+    classie.remove(itemElem, "is-hidden");
+    $container.packery('appended', itemElem);
+    return $container.packery();
+  });
+};
+
+removeItems = function() {
+  return $('.item').remove().promise();
 };
 
 createItems = function(results, container) {
+  var fragment;
+  fragment = document.createDocumentFragment();
+  return _.each(results, function(data) {
+    var img, item;
+    item = document.createElement("div");
+    item.className = "item is-hidden";
+    img = document.createElement("img");
+    img.src = data.images.downsized.url;
+    item.appendChild(img);
+    fragment.appendChild(item);
+    return container.append(fragment);
+  });
+};
+
+randomItems = function(results, container) {
   var fragment;
   fragment = document.createDocumentFragment();
   return _.each(results, function(response) {
@@ -20,43 +52,53 @@ createItems = function(results, container) {
     item = document.createElement("div");
     item.className = "item is-hidden";
     img = document.createElement("img");
-    console.log(response);
     img.src = response.data.image_url;
     item.appendChild(img);
     fragment.appendChild(item);
-    return container.appendChild(fragment);
+    return container.append(fragment);
   });
 };
 
-docReady(function() {
-  var container, pckry, promise, results;
+getTrending = function($container) {
+  var limit;
+  limit = 25;
+  return $.get("http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC&limit=" + limit).done(function(results) {
+    createItems(results.data, $container);
+    return loadImages($container);
+  });
+};
+
+getRandom = function($container) {
+  var promise, results;
   results = {};
-  container = document.querySelector("#container");
-  promise = $.when(_.times(20, function(n) {
+  promise = $.when(_.times(10, function(n) {
     return $.get("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC").done(function(data) {
       return results[n] = data;
     });
   }));
-  promise.done(function(data) {
+  return promise.done(function(data) {
     return $.when.apply($, data).done(function() {
-      var imgLoad;
-      createItems(results, container);
-      imgLoad = imagesLoaded(container);
-      return imgLoad.on("progress", function(imgLoad, image) {
-        var itemElem;
-        if (!image.isLoaded) {
-          return;
-        }
-        itemElem = image.img.parentNode;
-        classie.remove(itemElem, "is-hidden");
-        pckry.appended(itemElem);
-        return pckry.layout();
-      });
+      randomItems(results, $container);
+      return loadImages($container);
     });
   });
-  return pckry = new Packery(container, {
+};
+
+docReady(function() {
+  var $container;
+  $container = $('#container');
+  $('.trending').on('click', function() {
+    removeItems();
+    Loading.show();
+    return getTrending($container);
+  });
+  $('.random').on('click', function() {
+    removeItems();
+    Loading.show();
+    return getRandom($container);
+  });
+  return $container.packery({
     itemSelector: ".item",
-    gutter: 2,
-    "columnWidth": 196
+    gutter: 2
   });
 });
